221b59380b393cbb06bd91f20afe96160e41ab01
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-17 13:05:18 +1100

    Update book instance update #post

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -178,6 +178,59 @@ exports.bookinstance_update_get = function(req, res, next) {
 };
 
 // Handle bookinstance update on POST
-exports.bookinstance_update_post = function(req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance update POST");
+exports.bookinstance_update_post = function(req, res, next) {
+  //Sanitize id passed in.
+  req.sanitize("id").escape();
+  req.sanitize("id").trim();
+
+  req.checkBody("book", "Book must be specified").notEmpty(); //We won't force Alphanumeric, because book titles might have spaces.
+  req.checkBody("imprint", "Imprint must be specified").notEmpty();
+  // req
+  //   .checkBody("due_back", "Invalid date")
+  //   .optional({ checkFalsy: true })
+  //   .isDate();
+
+  req.sanitize("book").escape();
+  req.sanitize("imprint").escape();
+  req.sanitize("status").escape();
+  req.sanitize("book").trim();
+  req.sanitize("imprint").trim();
+  req.sanitize("status").trim();
+  req.sanitize("due_back").toDate();
+
+  var bookinstance = new BookInstance({
+    book: req.body.book,
+    imprint: req.body.imprint,
+    status: req.body.status,
+    due_back: req.body.due_back,
+    _id: req.params.id //This is required, or a new ID will be assigned!
+  });
+
+  var errors = req.validationErrors();
+  if (errors) {
+    Book.find({}, "title").exec(function(err, books) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        book_list: books,
+        selected_book: bookinstance.book._id,
+        errors: errors,
+        bookinstance: bookinstance
+      });
+    });
+    return;
+  } else {
+    // Data from form is valid
+
+    BookInstance.findByIdAndUpdate(req.params.id, bookinstance, {}, function(err, thebookinstance) {
+      if (err) {
+        return next(err);
+      }
+      //successful - redirect to new book-instance record.
+      res.redirect(thebookinstance.url);
+    });
+  }
 };
